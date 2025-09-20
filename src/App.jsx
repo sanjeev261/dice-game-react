ebfa4bc75e3eac8420a09fcf6c756c810c260de8
sanjeev261 <sanjeevpal2601@gmail.com>
2025-09-20 17:24:27 +0530

    Initial commit - Dice Game React Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import StartGame from "./Component/StartGame";
+import GamePlay from "./component/GamePlay";
+
+function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
+  return (
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
+}
+
+export default App;
